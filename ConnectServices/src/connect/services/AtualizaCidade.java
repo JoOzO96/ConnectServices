559ac7896513e038f6b7e1ddfc60df8ae5296dbf
Jose <suporte@connectinfo.com.br>
2017-11-23 22:13:50 -0200

    Recebimento de atualizacoes de cliente, iniciado

---
+++ ConnectServices/src/connect/services/AtualizaCidade.java
@@ -0,0 +1,90 @@
+package connect.services;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import connect.Classes.Cidade;
+import connect.Classes.ControleCodigo;
+import connect.utils.FabricaConexao;
+import connect.utils.InsereDados;
+import connect.utils.InsereField;
+
+@Path("recebeCidadeAtualizada")
+public class AtualizaCidade {
+
+	@POST
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String recebeCidadeAtualizada(String cidade) throws ClassNotFoundException, SQLException, ParseException {
+		Gson gson = new GsonBuilder().setDateFormat("dd/MM/yyyy").create();
+		InsereField insereField = new InsereField();
+		Cidade cidades[] = gson.fromJson(cidade, Cidade[].class);
+		Cidade cidadeCadastrada = new Cidade();
+		List<ControleCodigo> listcontroleCodigo = new ArrayList<>();
+
+		List<Cidade> listCidade = new ArrayList<>(Arrays.asList(cidades));
+		Connection connection = FabricaConexao.getConnection();
+
+		for (int i = 0; i < listCidade.size(); i++) {
+			ControleCodigo controleCodigo = new ControleCodigo();
+			ResultSet resultSet = connection.createStatement()
+					.executeQuery("SELECT * FROM Cidade where [Cód Cidade] = " + listCidade.get(i).getCodcidade());
+			resultSet.next();
+			Cidade cidade2 = new Cidade();
+			List<Field> fields = cidade2.retornaLista();
+
+			for (int j = 0; j < fields.size(); j++) {
+
+				Object retorno = insereField.retornaField(fields.get(j), listCidade.get(i));
+
+				Object teste = insereField.insereField(fields.get(j), cidade2, retorno);
+				cidade2 = (Cidade) teste;
+			}
+
+			// GERA UM ARRAY CONTENDO O NOME DO CAMPO E OUTRO ARRAY COM O VALOR NA MESMA
+			// POSIÇÃO
+
+			List<Field> campos = insereField.retornaArrayCampos(cidade2);
+			List<String> dados = insereField.retornaArrayDados(cidade2, campos);
+
+			InsereDados insereDados = new InsereDados();
+			String insert = insereDados.retornaUpdate(campos, dados, "Cidade", "[cód cidade]", cidade2.getCodcidade().toString());
+			System.out.println(insert);
+			insert = insert.replaceAll("codcidade", "[Cód Cidade]");
+			insert = insert.replaceAll("nomecidade", "[Nome Cidade]");
+			connection.createStatement().executeUpdate(insert);
+
+			resultSet = connection.createStatement()
+					.executeQuery("SELECT TOP 1 * FROM Cidade order by [Cód Cidade] desc");
+			if (resultSet.next()) {
+				if (resultSet.getLong("Cód Cidade") != cidade2.getCodcidade()) {
+					controleCodigo.setCodigoAndroid(cidade2.getCodcidade());
+					controleCodigo.setCodigoBanco(resultSet.getLong("Cód Cidade"));
+					listcontroleCodigo.add(controleCodigo);
+				}
+			}
+
+		}
+
+		if (listcontroleCodigo.size() > 0) {
+			return gson.toJson(listcontroleCodigo);
+		} else {
+			return "";
+		}
+	}
+}
---
+++ ConnectServices/src/connect/services/AtualizaCliente.java
@@ -0,0 +1,91 @@
+package connect.services;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+
+import connect.Classes.Cliente;
+import connect.Classes.ControleCodigo;
+import connect.utils.FabricaConexao;
+import connect.utils.InsereDados;
+import connect.utils.InsereField;
+
+@Path("recebeCliente")
+public class AtualizaCliente {
+	
+	@POST
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String recebeCliente(String cliente) throws ClassNotFoundException, SQLException, ParseException {
+		Gson gson = new Gson();
+		InsereField insereField = new InsereField();
+		Cliente clientes[] = gson.fromJson(cliente, Cliente[].class);
+		Cliente clienteCadastrado = new Cliente();
+		List<ControleCodigo> listcontroleCodigo = new ArrayList<>();
+		List<Cliente> listCliente = new ArrayList<>(Arrays.asList(clientes));
+		Connection connection = FabricaConexao.getConnection();
+
+		for (int i = 0; i < listCliente.size(); i++) {
+			ControleCodigo controleCodigo = new ControleCodigo();
+			ResultSet resultSet = connection.createStatement()
+					.executeQuery("SELECT * FROM Cliente where [Código] = " + listCliente.get(i).getCodigo());
+			resultSet.next();
+				Cliente cliente1 = new Cliente();
+				List<Field> fields = new ArrayList<>(Arrays.asList(cliente1.getClass().getDeclaredFields()));
+
+				for (int j = 0; j < fields.size(); j++) {
+
+					Object retorno = insereField.retornaField(fields.get(j), listCliente.get(i));
+
+					Object teste = insereField.insereField(fields.get(j), cliente1, retorno);
+					cliente1 = (Cliente) teste;
+				}
+				
+				// GERA UM ARRAY CONTENDO O NOME DO CAMPO E OUTRO ARRAY COM O VALOR NA MESMA POSIÇÃO
+				
+				List<Field> campos = insereField.retornaArrayCampos(cliente1);
+				List<String> dados = insereField.retornaArrayDados(cliente1, campos);
+
+				InsereDados insereDados = new InsereDados();
+				String insert = insereDados.retornaUpdate(campos, dados, "Cliente", "[Código]", cliente1.getCodigo().toString());
+				insert = insert.replaceAll("codigo", "[Código]");
+				insert = insert.replaceAll("nomecliente", "[nome cliente]");
+				insert = insert.replaceAll("codprofissao", "[Cód Profissao]");
+				insert = insert.replaceAll("codcidade", "[Cod Cidade]");
+				insert = insert.replaceAll("codvendedor", "[Cód Vendedor]");
+				insert = insert.replaceAll("responsavel", "[Responsável]");
+				insert = insert.replaceAll("conjuge", "[Conjugê]");
+				insert = insert.replace("[Código]pgto", "[CódigoPgto]");
+//				System.out.println(insert);
+				connection.createStatement().executeUpdate(insert);
+				
+				resultSet = connection.createStatement()
+						.executeQuery("SELECT TOP 1 * FROM Cliente order by [Código] desc");
+				if (resultSet.next()){
+						controleCodigo.setCodigoAndroid(cliente1.getCodigo());
+						controleCodigo.setCodigoBanco(resultSet.getLong("Código"));
+						listcontroleCodigo.add(controleCodigo);
+				}
+			}
+		
+		if (listcontroleCodigo.size() > 0){
+			return gson.toJson(listcontroleCodigo);
+		}else{
+			return "";
+		}
+	}
+
+}
